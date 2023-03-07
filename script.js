daa257b025cc81da116f3c71ec49121f07c594a7
BryanWillian <--global>
2023-03-07 20:49:50 -0300

    exemplo de saídas no console

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-primeira linha
+//primeira linha
+
+/**
+ * 
+ */
+
+//Amostra de saída do console com ponteiros de marcação.
+console.log('Olá mundo')
+console.error('Deu erro')
+console.warn('atenção')
